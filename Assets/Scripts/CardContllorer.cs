448f288cc860efd6938e654f7e316070ece794f9
DESKTOP-ILB6005\User <hmj2622010@stu.o-hara.ac.jp>
2026-08-24 06:52:20 +0900

    完成間近　あとは細かい調整と効果音

--- Assets/Scripts/CardContllorer.cs
+++ Assets/Scripts/CardContllorer.cs
@@ -1,67 +1,113 @@
+using TMPro;
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CardContllorer : MonoBehaviour
 {
-	[SerializeField] int startCard;
-	float cardLocation = 0;
-	int timer = 0;
-	int cardNumber = 5;
-	int cardNow = 3;
+	[SerializeField] GameObject cardScoreText;
+	[SerializeField] GameObject cardScoreUI;
+	[SerializeField] GameObject youWin;
+	[SerializeField] GameObject youLose;
+	
+	[SerializeField] int cardStart;
 
+	[SerializeField] NumberController NumberController;
+
+	float cardLocation = 0;	// カードの場所(角度)
+	int timer = 0;
+	int cardNumber = 5;	// カードの数
+	int cardNow = 3;    // 現在のカードの番号
+	int cSpacing = 40; // カードの間隔
+	float selectXY = 0; // カードの選択時に動く座標
+	
+	int cardScore = 0; // このカードのスコア
+
+	bool select = false;
 	bool plus = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
+		Application.targetFrameRate = 60;
+		cardScore = Random.Range(40,80);
+	}
 
     // Update is called once per frame
     void Update()
     {
-		timer--;
-		transform.rotation = Quaternion.Euler(0, 0, startCard * 35 + cardLocation * 35);
-		if(startCard + cardLocation>=-1.1&& -0.9>=startCard + cardLocation) 
+		if (select == false)
 		{
-			transform.position = new Vector3(-3,-4.5f, 0);
-		}
-		else transform.position = new Vector3(-4,-5.5f, 0);
-		//transform.Rotate(new Vector3(0, 0, startCard * 35 + cardLocation * 35));
+			if(NumberController.answerScore==0&&Random.Range(1,15)==4) NumberController.answerScore = cardScore;	// まだ答えが決まっていない場合に設定する
 
-		if(startCard>cardNumber/2|| startCard < -cardNumber / 2) 
-		{
-			gameObject.SetActive(false);
-		}
-		else gameObject.SetActive(true);
-
-		if (timer <= 0)
-		{
-			if (timer == 0)
+			timer--;
+			transform.rotation = Quaternion.Euler(0, 0, cardStart * cSpacing + cardLocation * cSpacing);	// 角度を反映
+			if (cardStart + cardLocation >= -1.2 && -0.8 >= cardStart + cardLocation)   // カードが正面に来たとき
 			{
-				if (plus == true) cardNow++;
-				else cardNow--;
-			}
-			if (Keyboard.current.dKey.wasPressedThisFrame)
-			{
-				if (cardNow < cardNumber-1)
+				cardScoreText.GetComponent<TextMeshProUGUI>().text = cardScore.ToString();
+				transform.position = new Vector3(-7, -5, 0);
+				if (Keyboard.current.spaceKey.wasReleasedThisFrame)
 				{
-					plus = true;
-					timer = 100;
+					cardScoreText.SetActive(false);
+					cardScoreUI.SetActive(false);
+					Debug.Log("カードを選択しました");
+					select = true;
 				}
 			}
-			if (Keyboard.current.sKey.wasPressedThisFrame)
+			else transform.position = new Vector3(-9, -7f, 0);
+			//transform.Rotate(new Vector3(0, 0, startCard * 35 + cardLocation * 35));
+
+			if (cardStart > cardNumber / 2 || cardStart < -cardNumber / 2)  // カードの数を超えた場合は非表示にする(今回のプロジェクトでは未使用)
 			{
-				if (cardNow > 0)
+				gameObject.SetActive(false);
+			}
+			else gameObject.SetActive(true);
+
+			if (timer <= 0)	// カードの角度を変える処理達
+			{
+				if (timer == 0)
 				{
-					plus = false;
-					timer = 100;
+					if (plus == true) cardNow++;
+					else cardNow--;
 				}
+				if (Keyboard.current.dKey.wasPressedThisFrame)
+				{
+					if (cardNow < cardNumber - 1)
+					{
+						plus = true;
+						timer = 20;
+					}
+				}
+				if (Keyboard.current.sKey.wasPressedThisFrame)
+				{
+					if (cardNow > 0)
+					{
+						plus = false;
+						timer = 20;
+					}
+				}
+			}
+			else
+			{   // カードの回転処理
+				if (plus == false) cardLocation -= 0.05f;
+				if (plus == true) cardLocation += 0.05f;
 			}
 		}
 		else
-		{
-			if (plus == false) cardLocation -= 0.01f;
-			if (plus == true) cardLocation += 0.01f;
+		{   // カードを選択した後の処理
+			transform.position = new Vector3(-7+selectXY, -5 + selectXY, 0);
+			selectXY+=0.5f;
+			timer++;
+			if (timer > 50) 
+			{
+			if(NumberController.answerScore == cardScore) 
+				{
+					youWin.SetActive(true);
+				}
+			else
+				{
+					youLose.SetActive(true);
+				}
+			}
 		}
 	}
 }
---
+++ Assets/Scripts/NumberController.cs
@@ -0,0 +1,85 @@
+﻿using System.Threading;
+using TMPro;
+using UnityEngine;
+
+public class NumberController : MonoBehaviour
+{
+	[SerializeField] TextMeshProUGUI flashNum;
+	[SerializeField] TextMeshProUGUI flashMark;
+	[SerializeField] TextMeshProUGUI joker;
+
+	[SerializeField] int flashSpeed = 140;
+
+	public int answerScore = 0;
+	int flashScore = 0;
+	int flashNow = 0;
+	string stNum;
+	string stMark;
+
+	bool colorRed = true;	// 一回ごとにトランプの色を切り替えるためのフラグ
+
+	int timer = 0;
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		timer++;
+		if (answerScore > 0)
+		{
+			if (flashNow == answerScore&&timer>flashSpeed)
+			{
+				flashNum.gameObject.SetActive(false);
+				flashMark.gameObject.SetActive(false);
+				joker.gameObject.SetActive(true);
+			}
+			else if (timer >flashSpeed)
+			{
+				flashScore = Random.Range(1, 14);   // 数字決め　目標値を上回っていた場合やトランプに合わせて変更する
+				if (flashNow + flashScore > answerScore) flashScore = answerScore - flashNow;
+				stNum = flashScore.ToString();
+				if (flashScore == 1) stNum = "A";
+				if (flashScore == 10) stNum = "10";
+				if (flashScore == 11) stNum = "J";
+				if (flashScore == 12) stNum = "Q";
+				if (flashScore == 13) stNum = "K";
+
+				if (colorRed)
+				{ // トランプの種類決め
+					if (Random.Range(1, 2) == 1)   
+					{ flashNum.color = Color.red; flashMark.color = Color.red; stMark = "♥"; }
+					else { flashNum.color = Color.red; flashMark.color = Color.red; stMark = "♦"; }
+					colorRed = false;
+				}
+				else { 					
+					if (Random.Range(1, 2) == 1)
+					{ flashNum.color = Color.black; flashMark.color = Color.black; stMark = "♠"; }
+					else { flashNum.color = Color.black; flashMark.color = Color.black; stMark = "♣"; }
+					colorRed = true;
+				}
+
+				flashNow += flashScore;
+
+				flashNum.GetComponent<TextMeshProUGUI>().text = stNum.ToString();
+				flashMark.GetComponent<TextMeshProUGUI>().text = stMark.ToString();
+
+				flashNum.gameObject.SetActive(true);
+				flashMark.gameObject.SetActive(true);
+				joker.gameObject.SetActive(false);
+
+				timer = 0;
+			}
+		}
+		else
+		{
+			flashNum.gameObject.SetActive(false);
+			flashMark.gameObject.SetActive(false);
+			joker.gameObject.SetActive(true);
+		}
+	}
+}
